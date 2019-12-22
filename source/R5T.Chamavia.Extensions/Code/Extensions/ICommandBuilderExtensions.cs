d44ce13b3ffedfc3bf979f1dcd6fc5baab8fe63a
MinexAutomation <minex.automation@gmail.com>
2019-12-21 23:51:18 -0800

    Added AddConfigurations() IConfigurationBuilder extension.

---
+++ source/R5T.Chamavia.Extensions/Code/Extensions/ICommandBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+
+namespace R5T.Chamavia.Extensions
+{
+    public static class ICommandBuilderExtensions
+    {
+        public static IConfigurationBuilder AddConfigurations(this IConfigurationBuilder configurationBuilder, Action<IConfigurationBuilder> action)
+        {
+            action(configurationBuilder);
+
+            return configurationBuilder;
+        }
+    }
+}
