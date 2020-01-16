b9f41280015536399e61cb3701b2cc3c6b8096ba
MinexAutomation <minex.automation@gmail.com>
2020-01-16 12:04:26 -0800

    Added ConfigurationHelper class. Added ConfigurationHelper.GetEmptyConfiguration() method.

---
+++ source/R5T.Chamavia.Extensions/Code/Classes/ConfigurationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+
+namespace R5T.Chamavia
+{
+    public static class ConfigurationHelper
+    {
+        public static IConfiguration GetEmptyConfiguration()
+        {
+            var emptyConfiguration = new ConfigurationBuilder()
+                .Build();
+
+            return emptyConfiguration;
+        }
+    }
+}
