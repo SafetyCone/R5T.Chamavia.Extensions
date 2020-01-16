d15f6278a78d1b7c4718f3d3aa89aaacbc52de50
MinexAutomation <minex.automation@gmail.com>
2020-01-16 12:01:34 -0800

    Fixed name of IConfigurationBuilderExtensions.

--- source/R5T.Chamavia.Extensions/Code/Extensions/IConfigurationBuilderExtensions.cs
+++ source/R5T.Chamavia.Extensions/Code/Extensions/IConfigurationBuilderExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace R5T.Chamavia.Extensions
 {
-    public static class ICommandBuilderExtensions
+    public static class IConfigurationBuilderExtensions
     {
         public static IConfigurationBuilder AddConfigurations(this IConfigurationBuilder configurationBuilder, Action<IConfigurationBuilder> action)
         {
