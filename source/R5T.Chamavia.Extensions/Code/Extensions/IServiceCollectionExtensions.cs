88291fcc987ea61ee561c0b04e38aa27668d9538
MinexAutomation <minex.automation@gmail.com>
2020-03-25 22:53:01 -0700

    Added AddConfiguration() IServiceCollection extension.

---
+++ source/R5T.Chamavia.Extensions/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Chamavia
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// The standard way of adding an <see cref="IConfiguration"/> instance as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IConfiguration>(configuration);
+
+            return services;
+        }
+    }
+}
