bbf40fc480ac986d70b286652f43ffd04ac3d1d0
David Coats <david@getsafetycone.com>
2021-05-17 22:28:15 -0700

    Replace R5T.Dacia with R5T.Dacia.Extensions project-reference dependency. Add AddX() method for IConfiguration.

--- source/R5T.Chamavia.Extensions/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Chamavia.Extensions/Code/Extensions/IServiceCollectionExtensions.cs
@@ -3,6 +3,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
+
 
 namespace R5T.Chamavia
 {
@@ -16,6 +18,15 @@ namespace R5T.Chamavia
             services.AddSingleton<IConfiguration>(configuration);
 
             return services;
+        }
+
+        /// <summary>
+        /// The standard way of adding an <see cref="IConfiguration"/> instance as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IConfiguration> AddConfigurationAction(this IServiceCollection services, IConfiguration configuration)
+        {
+            var serviceAction = ServiceAction.New<IConfiguration>(() => services.AddConfiguration(configuration));
+            return serviceAction;
         }
     }
 }
