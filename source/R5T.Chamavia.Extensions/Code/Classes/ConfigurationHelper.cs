386c257ced1c34f17166443ae0ec3d9cf98ae996
David Coats <david@getsafetycone.com>
2021-06-24 18:59:05 -0700

    Add IConfiguration extensions for getting values and determining if a value was found.

--- source/R5T.Chamavia.Extensions/Code/Classes/ConfigurationHelper.cs
+++ source/R5T.Chamavia.Extensions/Code/Classes/ConfigurationHelper.cs
@@ -7,6 +7,21 @@ namespace R5T.Chamavia
 {
     public static class ConfigurationHelper
     {
+        public const string NotFoundValue = null;
+
+
+        public static bool ValueWasFound(string value)
+        {
+            var output = value != ConfigurationHelper.NotFoundValue;
+            return output;
+        }
+
+        public static bool ValueNotFound(string value)
+        {
+            var output = value == ConfigurationHelper.NotFoundValue;
+            return output;
+        }
+
         public static IConfiguration GetEmptyConfiguration()
         {
             var emptyConfiguration = new ConfigurationBuilder()
---
+++ source/R5T.Chamavia.Extensions/Code/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+
+using R5T.Chamavia;
+using R5T.Magyar;
+
+
+namespace System
+{
+    public static class IConfigurationExtensions
+    {
+        public static WasFound<string> GetValueOkIfNotExists(this IConfiguration configuration, string key)
+        {
+            var value = configuration[key];
+
+            var exists = ConfigurationHelper.ValueWasFound(value);
+
+            var wasFound = WasFound.From(exists, value);
+            return wasFound;
+        }
+
+        public static string GetRequiredValue(this IConfiguration configuration, string key)
+        {
+            var value = configuration[key];
+
+            if(ConfigurationHelper.ValueNotFound(value))
+            {
+                throw new ArgumentException($"Configuration value not found for key '{key}'.");
+            }
+
+            return value;
+        }
+    }
+}
